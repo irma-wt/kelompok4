ca4a2009ff4b44591e301d4027136560a354714e
irma <irmawati@mhs.itenas.ac.id>
2025-01-08 19:11:23 +0700

    percobaan

---
+++ Kelompok4_DailyPlanner/src/main/java/com/mycompany/kelompok4_dailyplanner/Kelompok4_DailyPlanner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kelompok4_dailyplanner;
+
+/**
+ *
+ * @author user
+ */
+public class Kelompok4_DailyPlanner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
