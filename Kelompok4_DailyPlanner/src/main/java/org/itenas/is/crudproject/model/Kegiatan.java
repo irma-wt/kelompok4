bdaee47434dcb96c52f7ed28c9c17ebaa7f74f03
irma <irmawati@mhs.itenas.ac.id>
2025-01-10 11:04:15 +0700

    formRegister selesai

--- Kelompok4_DailyPlanner/src/main/java/org/itenas/is/crudproject/model/Kegiatan.java
+++ Kelompok4_DailyPlanner/src/main/java/org/itenas/is/crudproject/model/Kegiatan.java
@@ -4,10 +4,62 @@
  */
 package org.itenas.is.crudproject.model;
 
+import javax.swing.JComboBox;
+
 /**
  *
  * @author user
  */
 public class Kegiatan {
+
+    private String jam;
+    private String aktivitas;
+    private String prioritas;
+    private String status;
+    // Deklaprasi JComboBox
+
+    public Kegiatan() {
+    }
+
+    public Kegiatan(String jam, String aktivitas, String prioritas, String status) {
+        this.jam = jam;
+        this.aktivitas = aktivitas;
+        this.prioritas = prioritas;
+        this.status = status;
+    }
+
+    public String getJam() {
+        return jam;
+    }
+
+    public void setJam(String jam) {
+        this.jam = jam;
+    }
+
+    public String getAktivitas() {
+        return aktivitas;
+    }
+
+    public void setAktivitas(String aktivitas) {
+        this.aktivitas = aktivitas;
+    }
+
+    public String getPrioritas() {
+        return prioritas;
+    }
+
+    public void setPrioritas(String prioritas) {
+        this.prioritas = prioritas;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     
+
 }
--- Kelompok4_DailyPlanner/src/main/java/org/itenas/is/crudproject/services/CrudService.java
+++ Kelompok4_DailyPlanner/src/main/java/org/itenas/is/crudproject/services/CrudService.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package org.itenas.is.crudproject.services;
-
+ import java.util.List;
+import org.itenas.is.crudproject.model.Kegiatan;
 /**
  *
  * @author user
  */
-public interface CrudService {
-    
+
+   
+
+public interface CrudService<T> {
+    // Membuat data baru
+    public boolean create(final Kegiatan object);
+
+    // Mencari satu data berdasarkan prioritas
+    T findByPrioritas(String prioritas);
+
+    // Mencari satu data berdasarkan jam
+    T findByJam(String jam);
+
+    // Mencari satu data berdasarkan aktivitas
+    T findByAktivitas(String aktivitas);
+
+    // Mendapatkan semua data
+    List<T> findAll();
+
+    // Mengupdate data berdasarkan aktivitas
+    void update(String aktivitas, T object);
+
+    // Menghapus data berdasarkan aktivitas
+    boolean delete(String aktivitas);
 }
+
+
